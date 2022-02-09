9d46ccca282ab107b246ca4be422393fc7a5fd6f
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2022-02-09 01:27:28 -0300

    Ja postou user

---
+++ README.md
@@ -0,0 +1,20 @@
+### ToDoApp
+
+---
+
+### Requisitos
+
+- [X] Should be able to create a new user
+- [] Should be able to list all user's todos
+- [] Should be able to create a new todo
+- [] Should be able to update a todo
+- [] Should be able to mark a todo as done
+- [] Should be able to delete a todo
+
+
+
+### Regras de negócio
+
+- [X] Should not be able to create a new user when username already exists
+- [] Should not be able to update a non existing todo
+- [] Should not be able to delete a non existing todo
--- src/index.js
+++ src/index.js
@@ -1,29 +1,89 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.filter((user) => user.username === username)
+
+  if (!user)
+    return response.status(400).json({error: 'Username already exists!'}) 
+
+  request.
+
+  console.log(user);
+
+  return next();
+
+  /*
+  
+  confere se tem username
+
+  {
+	  error: 'Mensagem do erro'
+  }
+  or
+
+  request.user = user;
+  
+  return user
+  */
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const checkIfUsernameAlreadyExists = users.find((user) => user.username === username)
+  
+  if (checkIfUsernameAlreadyExists)
+    return response.status(400).json({error: 'Username already exists!'})
+
+  const newUser = {
+    id: uuidv4(),
+	  name, 
+	  username, 
+	  todos: []  
+  }
+
+  users.push(newUser);
+  return response.status(200).send();
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  console.log( user );
+  return response.send('teste')
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  /*
+    { 
+	    id: 'uuid', // precisa ser um uuid
+	    title: 'Nome da tarefa',
+	    done: false, 
+	    deadline: '2021-02-27T00:00:00.000Z', 
+	    created_at: '2021-02-22T00:00:00.000Z'
+    } 
+    { 
+	    id: 'uuid', // precisa ser um uuid
+	    title: 'Nome da tarefa',
+	    done: false, 
+	    deadline: new Date(deadline), 
+	    created_at: new Date()
+    }
+  */
+
+  
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
